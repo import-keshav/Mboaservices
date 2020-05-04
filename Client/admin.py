a22051cf8f159cf070a1ff15e5154e425ba74676
Keshav Bathla <keshavbathla2017@gmail.com>
2020-05-04 13:51:20 +0530

    refreshed gitignore

--- Client/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-from .models import (
-	Client, ClientCart, ClientNotification
-)
-
-admin.site.register(Client)
-admin.site.register(ClientCart)
-admin.site.register(ClientNotification)
