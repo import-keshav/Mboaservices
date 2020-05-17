d483b7f0d04aea796d9645ceed7a5d4207edd711
Keshav Bathla <keshavbathla2017@gmail.com>
2020-05-17 11:17:20 +0530

    Add JWT

--- Mboaservices/settings.py
+++ Mboaservices/settings.py
@@ -60,7 +60,12 @@ MIDDLEWARE = [
 ]
 
 REST_FRAMEWORK = {
-    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend']
+    'DEFAULT_FILTER_BACKENDS': [
+        'django_filters.rest_framework.DjangoFilterBackend'
+    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ],
 }
 
 ROOT_URLCONF = 'Mboaservices.urls'
--- User/urls.py
+++ User/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from rest_framework_simplejwt import views as jwt_views
 
 from . import views
 
@@ -8,4 +9,7 @@ urlpatterns = [
 	path('change-password', views.ChangePassword.as_view()),
 	path('restaurant-login', views.RestraurantLogin.as_view()),
 	path('update-user/<int:pk>', views.UserUpdateProfile.as_view()),
+
+    path('api/token/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
 ]
