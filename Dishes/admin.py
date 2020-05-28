7a80909a8d4d2a253414a1a60d66712bbdbfe1f2
Keshav Bathla <keshavbathla2017@gmail.com>
2020-05-28 22:35:17 +0530

    Add Filter In Dishes Admin Panel

--- Dishes/admin.py
+++ Dishes/admin.py
@@ -13,6 +13,7 @@ class DishAddOnsTabularInline(admin.TabularInline):
 class DishAdmin(admin.ModelAdmin):
 	list_display = ('name', 'restaurant', 'price', 'is_available', 'description' , 'image', 'get_category', 'id')
 	search_fields = ('name', 'restaurant__name', 'price', 'restaurant__id', 'id')
+	list_filter = ('name', 'restaurant', 'categories')
 	inlines = (DishAddOnsTabularInline,)
 	def get_category(self, obj):
 		return [cat.name for cat in obj.categories.all()]
@@ -24,5 +25,6 @@ class DishAddOnsAdmin(admin.ModelAdmin):
 	list_display = ('name', 'dish', 'is_free', 'price', 'id')
 	search_fields = ('name', 'dish__name')
 	raw_id_fields = ('dish',)
+	list_filter = ('name', 'dish', 'is_free')
 
 
