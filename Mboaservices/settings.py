2419653b06861b90d5129e36ce154af631a2b51d
Keshav Bathla <keshavbathla2017@gmail.com>
2020-08-21 12:58:54 +0530

    Change Database Engine

--- Mboaservices/settings.py
+++ Mboaservices/settings.py
@@ -95,23 +95,23 @@ WSGI_APPLICATION = 'Mboaservices.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': 'mboaservices',
-#         'USER': 'admin',
-#         'PASSWORD': 'XbU9Q7n3RxbXRYTZ',
-#         'HOST': 'localhost',
-#         'PORT': '',
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'mboaservices',
+        'USER': 'admin',
+        'PASSWORD': 'XbU9Q7n3RxbXRYTZ',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
