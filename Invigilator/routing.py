f09bdbb7d37e17f7c7ae5e7841a3466f614bacec
Keshav Bathla <keshavbathla2017@gmail.com>
2020-08-22 15:19:19 +0530

    Added New Routing Url

--- Invigilator/routing.py
+++ Invigilator/routing.py
@@ -4,7 +4,7 @@ from django.urls import path
 from . import consumers
 
 websocket_urlpatterns = [
-	path('invigilator/chat/<str:room_name>', consumers.ClientToInvigilator),
-	path('invigilator/chat/<str:room_name>/<str:person>', consumers.ClientsToInvigilator),
-	path('invigilator/get-incoming-order/<int:invigilator_id>', consumers.GetIncomingOrderForInvigilator),
+	path('wss/invigilator/chat/<str:room_name>', consumers.ClientToInvigilator),
+	path('wss/invigilator/chat/<str:room_name>/<str:person>', consumers.ClientsToInvigilator),
+	path('wss/invigilator/get-incoming-order/<int:invigilator_id>', consumers.GetIncomingOrderForInvigilator),
 ]
