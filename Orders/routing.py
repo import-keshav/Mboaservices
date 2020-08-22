718d8de6479b0b62605218ba38f3f9d2b6dfa61b
Keshav Bathla <keshavbathla2017@gmail.com>
2020-08-22 15:15:11 +0530

    Testing Commit

--- Orders/routing.py
+++ Orders/routing.py
@@ -4,6 +4,6 @@ from django.urls import path
 from . import consumers as orders_consumers
 
 websocket_urlpatterns = [
-    path('orders/get-incoming-orders/<int:pk>', orders_consumers.IncomingRestaurantOrders),
-    path('orders/update-get-order-status/<int:pk>', orders_consumers.UpdateGetOrderStatus),
+    path('wss/orders/get-incoming-orders/<int:pk>', orders_consumers.IncomingRestaurantOrders),
+    path('wss/orders/update-get-order-status/<int:pk>', orders_consumers.UpdateGetOrderStatus),
 ]
