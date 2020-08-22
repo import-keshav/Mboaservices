67e7cf570770c9a3b76eda31a6e7100b4bda10fc
Keshav Bathla <keshavbathla2017@gmail.com>
2020-08-22 15:10:57 +0530

    Back To Previous State of websocket routing

--- Orders/routing.py
+++ Orders/routing.py
@@ -4,6 +4,6 @@ from django.urls import path
 from . import consumers as orders_consumers
 
 websocket_urlpatterns = [
-    path('wss/orders/get-incoming-orders/<int:pk>', orders_consumers.IncomingRestaurantOrders),
-    path('wss/orders/update-get-order-status/<int:pk>', orders_consumers.UpdateGetOrderStatus),
+    path('orders/get-incoming-orders/<int:pk>', orders_consumers.IncomingRestaurantOrders),
+    path('orders/update-get-order-status/<int:pk>', orders_consumers.UpdateGetOrderStatus),
 ]
