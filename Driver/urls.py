92944d3b6d52c02f07f22094bf21498151195b7a
Keshav Bathla <keshavbathla2017@gmail.com>
2020-10-01 17:35:55 +0530

    Added API for getting restaurant Drivers

--- Driver/urls.py
+++ Driver/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-	path('get-driver', views.GetRestaurantDriver.as_view()),
+	path('get-driver', views.GetDrivers.as_view()),
+	path('get-restaurant-driver/<int:pk>', views.GetRestaurantDrivers.as_view()),
 ]
--- Driver/views.py
+++ Driver/views.py
@@ -6,7 +6,15 @@ from . import serializers as driver_serializers
 from . import models as driver_models
 
 
-class GetRestaurantDriver(generics.ListAPIView):
+class GetDrivers(generics.ListAPIView):
     renderer_classes = [JSONRenderer]
     serializer_class = driver_serializers.DriverGetSerializer
     queryset = driver_models.Driver.objects.all()
+
+
+class GetRestaurantDrivers(generics.ListAPIView):
+    renderer_classes = [JSONRenderer]
+    serializer_class = driver_serializers.DriverGetSerializer
+
+    def get_queryset(self):
+    	return [driver.driver for driver in driver_models.DriverRestaurant.objects.filter(restaurant__pk=self.kwargs['pk'])]
